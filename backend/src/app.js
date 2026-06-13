d5a308f37f99c9bdb9d0917cf74312079a027820
askdubem <chidubemokakpu@gmail.com>
2026-06-13 13:19:19 +0100

    Initial commit - fashion store project

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 
 const app = express();
-
+//
 app.use(helmet());
 app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
